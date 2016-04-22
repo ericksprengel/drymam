b0d654bbf76440b1cfe1a6e832fa2fde45019655
Erick Massa Sprengel <erick.sprengel@gmail.com>
2016-04-22 00:10:37 -0300

    new screenshot tool version

--- screenshot.py
+++ screenshot.py
@@ -1,15 +1,51 @@
+#!/usr/bin/env monkeyrunner
 # Imports the monkeyrunner modules used by this program
 from com.android.monkeyrunner import MonkeyRunner, MonkeyDevice
 from time import sleep
-import sys, os
+import os, sys
+
+# ALL VARS, PARAMS and OPTIONS INITIALIZATION
+IMG_PATH = None
+RECT     = None
 
 
-# Connects to the current device, returning a MonkeyDevice object
-device = MonkeyRunner.waitForConnection()
+# PARAMS AND OPTIONS PARSE
+# there is no argparse :(
 
-# Installs the Android package. Notice that this method returns a boolean, so you can test
-# to see if the installation worked.
-# device.installPackage('myproject/bin/MyApplication.apk')
+# ALL VARS, PARAMS and OPTIONS TREATMENTS
+if len(sys.argv) == 2:
+  IMG_PATH = sys.argv[1]
+  RECT = None
+elif len(sys.argv) == 6:
+  IMG_PATH = sys.argv[1]
+  RECT = tuple([ int(x) for x in sys.argv[2:] ])
+else:
+  print("usage: screenshot PATH [ X Y WIDTH HEIGHT ]\n")
+  sys.exit(1)
+
+# SCRIPT STARTS HERE
+
+try:
+  # Connects to the current device, returning a MonkeyDevice object
+  device = MonkeyRunner.waitForConnection()
+
+  
+  # Takes a screenshot
+  result = device.takeSnapshot()
+
+  # getting a sub image from snapshot?
+  if RECT:
+    result = result.getSubImage(RECT)
+
+  print(IMG_PATH)
+
+  # Writes the screenshot to a file
+  result.writeToFile(IMG_PATH,'png')
+  
+except KeyboardInterrupt:
+  # USE KEYBOARD INTERRUPT TO TREAT ^C
+  sys.exit("{0}Interrupted by user...{1}".format(bcolors.WARNING, bcolors.ENDC))
+
 
 # sets a variable with the package's internal name
 #package = 'com.example.testpixel'
@@ -33,11 +69,3 @@ device = MonkeyRunner.waitForConnection()
 # Presses the Menu button
 # device.press('KEYCODE_MENU', MonkeyDevice.DOWN_AND_UP)
 
-# Takes a screenshot
-result = device.takeSnapshot()
-
-# Output file image
-fileImage = os.path.join(os.path.dirname(os.sys.argv[0]), "shot1.png")
-
-# Writes the screenshot to a file
-result.writeToFile(fileImage,'png')
--- screenshot.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-monkeyrunner screenshot.py
