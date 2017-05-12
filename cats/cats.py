c30becaf9dbff6cbd73913266d66e2a5795eeba3
Erick M. Sprengel <erick.sprengel@gmail.com>
2017-05-12 16:42:51 -0300

    add c.a.t.s.

---
+++ cats/cats.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env monkeyrunner
+# -*- coding: utf-8 -*-
+# Imports the monkeyrunner modules used by this program
+from com.android.monkeyrunner import MonkeyRunner, MonkeyDevice
+import sys, os
+import random
+import datetime
+
+class areas:
+# Press "combate rápido"
+  FAST_FIGHT        = (( 730, 585),(1070, 680), "FAST_FIGHT")
+  START_FIGHT       = (( 430, 250),( 890, 530), "START_FIGHT")
+  OK_BUTTON_VICTORY = (( 570, 630),( 690, 690), "OK_BUTTON_VICTORY")
+  OK_BUTTON_DEFENSE = (( 590, 630),( 690, 620), "OK_BUTTON_DEFENSE")
+
+
+
+def save_screenshot(device, file_path):
+  # Takes a screenshot
+  result = device.takeSnapshot()
+  # Output file image
+  fileImage = os.path.join(os.path.dirname(os.sys.argv[0]), file_path)
+  # Writes the screenshot to a file
+  result.writeToFile(fileImage,'png')
+
+def tap_on_area(device, area, sleep, debug=False):
+  tap = (
+      random.randint(area[0][0], area[1][0]),
+      random.randint(area[0][1], area[1][1])
+    )
+  if debug:
+    save_screenshot(device, "%s_%s" % (datetime.datetime.now().strftime("%y-%m-%d_%H-%M-%f"), area[2]))
+    print("click: %s, %s\t\t%s" % (tap[0], tap[1], area[2]) )
+  device.touch(tap[0],tap[1],MonkeyDevice.DOWN_AND_UP)
+  MonkeyRunner.sleep(sleep)
+
+
+def iterate(device):
+  tap_on_area(device, areas.FAST_FIGHT, 3.0, True)
+  tap_on_area(device, areas.START_FIGHT, 15.0, True)
+  tap_on_area(device, areas.OK_BUTTON_VICTORY, 3.0, True)
+
+
+
+# Connects to the current device, returning a MonkeyDevice object
+device = MonkeyRunner.waitForConnection()
+
+#while True:
+for j in range(5):
+  print("ITERATION: %d" % (j) )
+  iterate(device)
