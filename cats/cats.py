63532df73937f6fa5968e096de77ece8b06cb744
Erick M. Sprengel <erick.sprengel@gmail.com>
2017-05-18 19:51:42 -0300

    [cats] add random for delay

--- cats/cats.py
+++ cats/cats.py
@@ -19,6 +19,9 @@ class areas:
   HEROES = [ (( 54 + 86*i - 10, 1045),( 54 + 86*i + 10, 1155)) for i in range(0,8) ]
 
 
+def has_button_to_dimiss(device):
+  print("TODO: has_button_to_dimiss")
+
 def save_screenshot(device, file_path):
   # Takes a screenshot
   result = device.takeSnapshot()
@@ -26,6 +29,7 @@ def save_screenshot(device, file_path):
   fileImage = os.path.join(os.path.dirname(os.sys.argv[0]), file_path)
   # Writes the screenshot to a file
   result.writeToFile(fileImage,'png')
+
 
 def tap_on_area(device, area, sleep, debug=False):
   tap = (
@@ -35,16 +39,16 @@ def tap_on_area(device, area, sleep, debug=False):
   if debug:
     now = datetime.datetime.now()
     # http://bugs.jython.org/issue2166
-    save_screenshot(device, "%s_%s_%s.png" % (now.strftime("%y-%m-%d_%H-%M"), now.microsecond, area[2]))
+    #save_screenshot(device, "%s_%s_%s.png" % (now.strftime("%y-%m-%d_%H-%M"), now.microsecond, area[2]))
     print("click: %s, %s\t\t%s" % (tap[0], tap[1], area[2]) )
   device.touch(tap[0],tap[1],MonkeyDevice.DOWN_AND_UP)
   MonkeyRunner.sleep(sleep)
 
 
 def iterate(device):
-  tap_on_area(device, areas.FAST_FIGHT, 3.0, True)
-  tap_on_area(device, areas.START_FIGHT, 15.0, True)
-  tap_on_area(device, areas.OK_BUTTON_VICTORY, 3.0, True)
+  tap_on_area(device, areas.FAST_FIGHT,         3.0 + random.random()*0.4, True)
+  tap_on_area(device, areas.START_FIGHT,       15.0 + random.random()*1.0, True)
+  tap_on_area(device, areas.OK_BUTTON_VICTORY,  3.0 + random.random()*0.4, True)
 
 
 
@@ -52,6 +56,6 @@ def iterate(device):
 device = MonkeyRunner.waitForConnection()
 
 #while True:
-for j in range(5):
+for j in range(100):
   print("ITERATION: %d" % (j) )
   iterate(device)
